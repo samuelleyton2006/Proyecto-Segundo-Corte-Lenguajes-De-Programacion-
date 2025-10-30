96ce80c40bd4ad9d5bda21349a1a3ea92f584991
Samuel Leyton Muñoz <145390285+samuelleyton2006@users.noreply.github.com>
2025-10-30 00:25:53 +0000

    Updated

--- pruebas/gramatica.py
+++ pruebas/gramatica.py
@@ -1,2 +1,6 @@
 """
- Willy le encanta el pene 
+ Willy le encanta el pensamiento lógico y ha decidido crear un programa en Python que
+ resuelva problemas de lógica utilizando gramáticas formales. El programa debe ser capaz de
+ definir una gramática, generar cadenas válidas según esa gramática y verificar si una cadena
+ dada pertenece al lenguaje definido por la gramática.      
+"""
